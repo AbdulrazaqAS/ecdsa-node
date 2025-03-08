5b284f34cbdefa700a5464ad9bd3893ffaae8d25
Abdulrazaq Ahmad Sabo <abdulrazaq2621@gmail.com>
2025-03-08 07:08:37 +0100

    working solution

--- server/index.js
+++ server/index.js
@@ -1,28 +1,12 @@
 const express = require("express");
+const { hexToBytes } = require("ethereum-cryptography/utils");
+const { verifySigner } = require("./scripts/crypto");
 const app = express();
 const cors = require("cors");
 const port = 3042;
 
 app.use(cors());
 app.use(express.json());
-
-accounts = [
-  {
-    privateKey: '5ac27dad168d26dfe5f2437f03f4cb44cef9f15cce20f2afba6517397f55f6b8',
-    publicKey: '032d70c07c1a66698a954515bae8f248513ca7dc5e58b77f78a6ea4070aabc95c1',
-    address: 'b7a921983823c0f9b5e59c19ad34a09102c0fb1a'
-  },
-  {
-    privateKey: '5a396d84599e28d03a05054c658864f0631902b92f1f3659faee2be1c83e5ae6',
-    publicKey: '0346d3088cabb62ed5111329fc4d4119376d404505858dd19fe059ae064b7475da',
-    address: '31c643d9ab9fcfbce0207313ac4d3c27fb3e482c'
-  },
-  {
-    privateKey: '2cd4bc1574ce3d998f91839e107c672fc8475fbf805c71dc6189735252674b92',
-    publicKey: '037a2f26669b088c6708f16262e5e1a5461ae3b10a6b85643099fbc70dd7863c38',
-    address: 'd185f62cbbedd2477d1348b8dae147bfeeb0f742'
-  }
-]
 
 const balances = {
   "b7a921983823c0f9b5e59c19ad34a09102c0fb1a": 100,
@@ -37,13 +21,32 @@ app.get("/balance/:address", (req, res) => {
 });
 
 app.post("/send", (req, res) => {
-  const { sender, recipient, amount } = req.body;
+  const { sender, recipient, amount, signature, recoveryBit, msgHash } = req.body;
+  let signerVerified;
+
+  // Catching Error: Cannot read properties of null (reading 'startsWith')
+  // initially when verifySigner params are in their defaults.
+  try {
+    signerVerified = verifySigner(signature, recoveryBit, msgHash);
+  } catch (error) {
+    console.error(error);
+  }
+
+  if (!signerVerified){
+    res.status(400).send({ message: "User not verified as the account owner"});
+    //console.error("User not verified as the account owner");
+    return;
+  }
+
 
   setInitialBalance(sender);
   setInitialBalance(recipient);
 
   if (balances[sender] < amount) {
     res.status(400).send({ message: "Not enough funds!" });
+  }
+  else if (amount <= 0){
+    res.status(400).send({ message: "Invalid amount!" });
   } else {
     balances[sender] -= amount;
     balances[recipient] += amount;
--- server/scripts/crypto.js
+++ server/scripts/crypto.js
@@ -1,7 +1,7 @@
 const { secp256k1 } = require("ethereum-cryptography/secp256k1");
 const { keccak256 } = require("ethereum-cryptography/keccak")
 const { getRandomBytesSync } = require('ethereum-cryptography/random');
-const { toHex } = require("ethereum-cryptography/utils");
+const { utf8ToBytes, toHex } = require("ethereum-cryptography/utils");
 
 function getAddress(publicKey){
 	// In ethereum, the address is the last 20 bytes of the keccak hash
@@ -10,7 +10,7 @@ function getAddress(publicKey){
 	const address = keccak256(publicKey.slice(1)).slice(-20);
 	//console.log("Address:", toHex(address));
 
-	return address;
+	return toHex(address);
 }
 
 function generateRandomAccount(){
@@ -30,4 +30,69 @@ function generateRandomAccount(){
 			};
 }
 
-module.exports = {generateRandomAccount, getAddress};
+function hashMessage(msg){
+	const messageBytes = utf8ToBytes(msg);
+	const messageHash = toHex(keccak256(messageBytes));
+	//console.log('Message Hash:', messageHash);
+	return messageHash;
+}
+
+function signMessageHash(msgHash, privateKey){
+	// https://github.com/paulmillr/noble-secp256k1#utils
+	const signature = secp256k1.sign(msgHash, privateKey); // returns a Signature class obj
+	//console.log('Signature:', signature.toCompactHex());
+
+	return signature;
+}
+
+// signature: hex, msgHash: hex
+function verifySigner(signature, recoveryBit, msgHash){
+	// https://github.com/paulmillr/noble-secp256k1#utils
+
+	const _signatureObj = secp256k1.Signature.fromCompact(signature); // recovery bit is undefined here
+	//console.log("secp256k1.Signature.fromCompact:", _signatureObj)
+	
+	const signatureObj = new secp256k1.Signature(_signatureObj.r, _signatureObj.s, recoveryBit);  // new Signature passing recoveryBit
+	//console.log("new secp256k1.Signature:", signatureObj)
+	
+	const publicKey = signatureObj.recoverPublicKey(msgHash).toHex();  // recoverPubKey returns ProjectivePoint obj
+	const isValid = secp256k1.verify(signatureObj, msgHash, publicKey);
+	//console.log('Is signature valid?', isValid);
+
+	return isValid;
+}
+
+
+/*
+A byte is 8 bits.
+One hex character represents 4 bits (also called a nibble).
+So, 2 hex characters = 8 bits = 1 byte.
+
+A typical Ethereum signature (65 bytes) is 130 chars in hex:
+65 bytes * 2 hex chars per byte = 130 hex characters
+0x [r (64 chars)] [s (64 chars)] [v (2 chars)]
+That's exactly 130 hex characters (or 132 if counting 0x).
+*/
+async function test (){
+	const msg = "AM the one";
+	const msgHash = hashMessage(msg);
+	const signed = signMessageHash(msgHash, "5a396d84599e28d03a05054c658864f0631902b92f1f3659faee2be1c83e5ae6"); // 2nd acct from README
+	const recoveryBit = signed.recovery
+	console.log(signed)
+	console.log(signed.toCompactHex()) // Recovery bit not included (128 hex chars: 64 bytes)
+	console.log(signed.toCompactRawBytes())  // Recovery bit not included (64 bytes)
+	console.log("\n")
+
+	// Converting to hex and not directly using the Signature obj because while adding the Signature
+	// obj to an Object to be passed from client to server, is says it can't serialize that. But it can
+	// serialize hex string. Using the Signature obj directly won't require recoveryBit bcoz it is already
+	// in the Signature.
+	const verify = verifySigner(signed.toCompactHex(), recoveryBit, msgHash);
+	console.log(verify);
+}
+
+// Also uncomment verifySigner commented lines if testing
+// test().then();
+
+module.exports = {generateRandomAccount, getAddress, hashMessage, signMessageHash, verifySigner};
+
